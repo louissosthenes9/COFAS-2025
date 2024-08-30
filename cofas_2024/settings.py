3468e410ee157743f7a86fe38bc00c1cf5fb9158
louissosthenes9 <louissosthenes9@gmail.com>
2024-08-30 10:12:42 +0300

    applications set up

--- cofas_2024/settings.py
+++ cofas_2024/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "applications.apps.ApplicationsConfig"
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,8 @@ ROOT_URLCONF = 'cofas_2024.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates']
+        ,
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
