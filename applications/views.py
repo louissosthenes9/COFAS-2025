a82fa81e91904c26a30b20a6bb23a022aabf33e9
KELVIN BYABATO <kelvinbyabato92@gmail.com>
2024-08-31 17:03:08 +0300

    Updated views.py to include the new forms.py
    any corrections?

--- applications/views.py
+++ applications/views.py
@@ -1,5 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import application_form
+
 
 # Create your views here.
+
 def form(request):
-    return render(request, 'form.html');
+    if request.method == 'POST':
+        form = application_form(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Application submitted successfully!')
+            return redirect('form')
+        else:
+            messages.error(request, 'Please correct the errors below.')
+    else:
+        form = application_form()
+    
+    return render(request, 'application/form.html', {'form': form})
