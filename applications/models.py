ef86e52b1557b764e5098ed290873aa613927c50
Louissosthenes9 <louissosthenes9@gmail.com>
2024-08-31 17:42:02 +0300

    added a validation function for extensions

--- applications/models.py
+++ applications/models.py
@@ -1,3 +1,6 @@
+import os
+
+from django.core.exceptions import ValidationError
 from django.db import models
 
 
@@ -26,6 +29,11 @@ class Application(models.Model):
         ('software', 'Software Development'),
         ('networking', 'Computer Networking'),
     ]
+    def validate_file_extension(value):
+        ext = os.path.splitext(value.name)[1]
+        valid_extensions = ['.pdf', '.doc', '.docx']
+        if not ext.lower() in valid_extensions:
+            raise ValidationError('Unsupported file extension.')
 
     first_name = models.CharField(max_length=100)
     middle_name = models.CharField(max_length=100, blank=True, null=True)
@@ -39,9 +47,10 @@ class Application(models.Model):
     year_of_study = models.CharField(max_length=2, choices=YEAR_CHOICES)
     unit = models.CharField(max_length=50, choices=UNIT_CHOICES)
     department = models.CharField(max_length=50, choices=DEPARTMENT_CHOICES)
-    reference_letter = models.FileField(upload_to='reference_letters/')
-    resume = models.FileField(upload_to='resumes/')
-    academic_transcripts = models.FileField(upload_to='transcripts/')
+    reference_letter = models.FileField(upload_to='reference_letters/',validators=[validate_file_extension])
+    resume = models.FileField(upload_to='resumes/',validators=[validate_file_extension])
+    academic_transcripts = models.FileField(upload_to='transcripts/',validators=[validate_file_extension])
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
+
