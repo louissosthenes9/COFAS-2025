fe5126a18094535acfcecc8907f95a63ca015339
Louissosthenes9 <louissosthenes9@gmail.com>
2024-09-01 15:09:19 +0300

    configurations files

---
+++ cofas_2024/celery.py
@@ -0,0 +1,23 @@
+from __future__ import absolute_import, unicode_literals
+import os
+from celery import Celery
+
+# Set the default Django settings module for the 'celery' program.
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cofas_2024.settings')
+
+app = Celery('cofas_2024')
+
+# Using a string here means the worker doesn't have to serialize
+# the configuration object to child processes.
+app.config_from_object('django.conf:settings', namespace='CELERY')
+
+# Load task modules from all registered Django apps.
+app.autodiscover_tasks()
+
+@app.task(bind=True)
+def debug_task(self):
+    print(f'Request: {self.request!r}')
+
+
+def shared_task():
+    return None
