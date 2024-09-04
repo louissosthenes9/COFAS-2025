86714633aec34ad6b66c1bfb794d8d4a0ab851d4
Louissosthenes9 <louissosthenes9@gmail.com>
2024-09-04 10:04:26 +0300

    fixed the renderred html error

--- applications/admin.py
+++ applications/admin.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
-from django.core.mail import send_mail
-from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.admin import GroupAdmin as BaseGroupAdmin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import User, Group
+from django.core.mail import EmailMultiAlternatives
+from django.template.loader import render_to_string
 from django.utils.html import format_html
+from django.utils.html import strip_tags
 from unfold.admin import ModelAdmin
+
 from .models import Application
 
 admin.site.unregister(User)
@@ -25,59 +28,56 @@ class GroupAdmin(BaseGroupAdmin, ModelAdmin):
 class ApplicationAdmin(ModelAdmin):
     actions = ['export_as_csv', 'approve_application', 'reject_application']
     list_display = ('first_name', 'last_name', 'gender', 'unit', 'department', 'status', 'action_buttons')
-    search_fields = ('first_name', 'last_name')
+    search_fields = ('first_name', 'last_name', 'email')
     list_filter = ('gender', 'level_of_study', 'unit', 'department', 'status')
 
     @admin.action(description='Export as CSV')
-    def export_as_csv(self, request, queryset):
+    def export_as_csv(self, queryset):
         import csv
         from django.http import HttpResponse
 
         response = HttpResponse(content_type='text/csv')
         response['Content-Disposition'] = 'attachment; filename=applications.csv'
         writer = csv.writer(response)
-        writer.writerow(['First Name', 'Last Name', 'Email', 'Gender', 'Level of Study', 'Unit', 'Department'])
+        writer.writerow(
+            ['First Name', 'Last Name', 'Email', 'Gender', 'Level of Study', 'Unit', 'Department', 'Status'])
 
         for application in queryset:
             writer.writerow([
                 application.first_name, application.last_name, application.email,
                 application.gender, application.level_of_study,
-                application.unit, application.department
+                application.unit, application.department, application.status
             ])
 
         return response
+
+    def _send_email(self, application, subject, template):
+        html_message = render_to_string(template)
+        plain_message = strip_tags(html_message)
+        email = EmailMultiAlternatives(
+            subject=subject,
+            body=plain_message,
+            from_email="admin@example.com",
+            to=[application.email]
+        )
+        email.attach_alternative(html_message, 'text/html')
+        email.send()
 
     @admin.action(description='Approve')
     def approve_application(self, request, queryset):
         for application in queryset:
             application.status = 'approved'
             application.save()
-            send_mail(
-                'Application Approved',
-                'Your application has been approved.',
-                'from@example.com',
-                [application.email],
-                fail_silently=False,
-            )
-        self.message_user(request, "Selected applications have been approved and emails sent.")
+            self._send_email(application, "Application Approved", 'emails/approvalemailtemplate.html')
+        self.message_user(request, f"{queryset.count()} application(s) have been approved and emails sent.")
 
     @admin.action(description='Reject')
     def reject_application(self, request, queryset):
         for application in queryset:
             application.status = 'rejected'
             application.save()
-            send_mail(
-                'Application Rejected',
-                'Your application has been rejected.',
-                'from@example.com',
-                [application.email],
-                fail_silently=False,
-            )
-        self.message_user(request, "Selected applications have been rejected and emails sent.")
-
-    @staticmethod
-    def view_application(obj):
-        return format_html('<a class="button" href="{}">View</a>', f"/admin/applications/application/{obj.pk}/")
+            self._send_email(application, "Application Rejected", 'emails/denialemailtemplate.html')
+        self.message_user(request, f"{queryset.count()} application(s) have been rejected and emails sent.")
 
     def action_buttons(self, obj):
         return format_html(
@@ -90,4 +90,4 @@ class ApplicationAdmin(ModelAdmin):
     action_buttons.allow_tags = True
 
     def get_list_display_links(self, request, list_display):
-        return ()  # This removes the default link from the first column
+        return None  # This removes the default link from the first column
